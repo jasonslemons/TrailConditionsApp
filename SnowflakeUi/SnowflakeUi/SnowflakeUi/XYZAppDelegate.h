ebbd28d232dab4e8f288b483b9a65a594f3ce3f3
Kyle Easterly <kyle.easterly@airsoftfix.com>
2014-08-13 18:13:41 -0800

    Commit of UI

---
+++ SnowflakeUi/SnowflakeUi/SnowflakeUi/XYZAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  XYZAppDelegate.h
+//  SnowflakeUi
+//
+//  Created by Kyle Easterly on 8/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface XYZAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SnowflakeUi/SnowflakeUi/SnowflakeUi/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SnowflakeUi
+//
+//  Created by Kyle Easterly on 8/13/14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "XYZAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([XYZAppDelegate class]));
+    }
+}
